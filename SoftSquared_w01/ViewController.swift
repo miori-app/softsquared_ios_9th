30649b2c47279216a57ad8885b69514ecc645be9
iammiori <mioriii@naver.com>
2020-07-24 00:13:14 +0900

    Initial Commit

---
+++ SoftSquared_w01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoftSquared_w01
+//
+//  Created by miori Lee on 2020/07/24.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
