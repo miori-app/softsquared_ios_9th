b4eaf606340ce63ec122b312dca2c83d2345bdab
iammiori <mioriii@naver.com>
2020-07-30 01:18:27 +0900

    Changed submodule to directory

---
+++ SoftSquared_w02/SoftSquared_w02/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  SoftSquared_w02
+//
+//  Created by miori Lee on 2020/07/30.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear")
+    }
+
+}
+
