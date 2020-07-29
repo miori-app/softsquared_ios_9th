20b5de8583c0163a218ad40153335aa407238be4
iammiori <mioriii@naver.com>
2020-07-30 01:29:28 +0900

    [feat] Add textfield and button and secondviewController

--- SoftSquared_w02/SoftSquared_w02/ViewController.swift
+++ SoftSquared_w02/SoftSquared_w02/ViewController.swift
@@ -10,25 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var unameTextFiled: UITextField!
+    @IBAction func didTapAction(_ sender: Any) {
+        let uname : String = unameTextFiled.text!
+        print(uname)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("first view  did load")
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        print("viewWillAppear")
+        print("1 : viewWillAppear")
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        print("viewDidAppear")
+        print("1 : viewDidAppear")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        print("viewWillDisappear")
+        print("1 : viewWillDisappear")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
-        print("viewDidDisappear")
+        print("1 : viewDidDisappear")
     }
 
 }
