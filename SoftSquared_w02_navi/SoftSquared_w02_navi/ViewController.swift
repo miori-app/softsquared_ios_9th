a0a35cc35c0e21adc1fd4b4293ca256a198868cb
iammiori <mioriii@naver.com>
2020-08-02 02:04:37 +0900

    [init] lifecycle using navigationbar

---
+++ SoftSquared_w02_navi/SoftSquared_w02_navi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoftSquared_w02_navi
+//
+//  Created by miori Lee on 2020/08/02.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SoftSquared_w02_navi/SoftSquared_w02_naviTests/SoftSquared_w02_naviTests.swift
@@ -0,0 +1,34 @@
+//
+//  SoftSquared_w02_naviTests.swift
+//  SoftSquared_w02_naviTests
+//
+//  Created by miori Lee on 2020/08/02.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import XCTest
+@testable import SoftSquared_w02_navi
+
+class SoftSquared_w02_naviTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
