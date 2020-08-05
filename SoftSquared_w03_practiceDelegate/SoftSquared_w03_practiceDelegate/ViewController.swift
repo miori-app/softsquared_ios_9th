ee6fd212fb9499447708d56c315d663ce9745876
iammiori <mioriii@naver.com>
2020-08-05 15:32:55 +0900

    [init] init week03 practice protocol, extension, delegate

---
+++ SoftSquared_w03_practiceDelegate/SoftSquared_w03_practiceDelegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoftSquared_w03_practiceDelegate
+//
+//  Created by miori Lee on 2020/08/05.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SoftSquared_w03_practiceDelegate/SoftSquared_w03_practiceDelegateTests/SoftSquared_w03_practiceDelegateTests.swift
@@ -0,0 +1,34 @@
+//
+//  SoftSquared_w03_practiceDelegateTests.swift
+//  SoftSquared_w03_practiceDelegateTests
+//
+//  Created by miori Lee on 2020/08/05.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import XCTest
+@testable import SoftSquared_w03_practiceDelegate
+
+class SoftSquared_w03_practiceDelegateTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
