606b7258ab4130ee66e8a6fc21864b5c8e9eaefb
iammiori <mioriii@naver.com>
2020-08-06 01:45:33 +0900

    [init] init tableview

---
+++ SoftSquared_w03_practiceTableView/SoftSquared_w03_practiceTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoftSquared_w03_practiceTableView
+//
+//  Created by miori Lee on 2020/08/06.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
