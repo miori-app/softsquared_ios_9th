ed28755606205a23ff609802fd1ec9554e46408b
iammiori <mioriii@naver.com>
2020-08-07 02:24:48 +0900

    [init] .gitignore

---
+++ SoftSquared_w03_wantedTable/SoftSquared_w03_wantedTable/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  SoftSquared_w03_wantedTable
+//
+//  Created by miori Lee on 2020/08/07.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SoftSquared_w03_wantedTable/SoftSquared_w03_wantedTable/CustomUITableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  CustomUITableViewCell.swift
+//  SoftSquared_w03_wantedTable
+//
+//  Created by miori Lee on 2020/08/07.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import Foundation
