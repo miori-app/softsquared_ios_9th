6d004f1b7f21799dfb5cfb5315ce8929b38368f9
iammiori <mioriii@naver.com>
2020-08-07 01:38:11 +0900

    [feat] Embed in navigationcontroller and tabbar controller

---
+++ SoftSquared_w03_wantedTable/SoftSquared_w03_wantedTable/TealViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TealViewController.swift
+//  SoftSquared_w03_wantedTable
+//
+//  Created by miori Lee on 2020/08/07.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import UIKit
+
+class TealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
