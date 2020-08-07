006efb9a84835258a2be3ad2c2a9cef7754a2ed4
iammiori <mioriii@naver.com>
2020-08-08 01:57:09 +0900

    [feat] Add popular company view (can pick)

---
+++ SoftSquared_w03_wantedTable/SoftSquared_w03_wantedTable/CompanyTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CompanyTableViewCell.swift
+//  SoftSquared_w03_wantedTable
+//
+//  Created by miori Lee on 2020/08/08.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import UIKit
+
+class CompanyTableViewCell : UITableViewCell {
+
+    @IBOutlet weak var cLogoImg: UIImageView!
+    @IBOutlet weak var cName: UILabel!
+    @IBOutlet weak var cKind: UILabel!
+    @IBOutlet weak var cFollow: UIButton!
+}
--- SoftSquared_w03_wantedTable/SoftSquared_w03_wantedTable/PickViewController.swift
+++ SoftSquared_w03_wantedTable/SoftSquared_w03_wantedTable/PickViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 
 class PickViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var cNames = ["카카오(KAKAO)","왓챠","쿠팡","NAVER Corp(네이버)","우아한형제들(배달의민족)","엔에이치엔(NHN)","딜리버리히어로 코리아 (Delivery Hero Korea)","원티드랩","화해(버드뷰)"]
+    var cLogos = ["kakao","watcha","coupang","naver","woowahan","nhn","deliveryhero","wanted","birdview"]
+    var cKinds = ["IT, 컨텐츠","IT, 컨텐츠","IT, 컨텐츠","IT, 컨텐츠","IT, 컨텐츠","IT, 컨텐츠","IT, 컨텐츠","IT, 컨텐츠","IT, 컨텐츠"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = "인기 회사"
+         
+        tableView.dataSource = self
+        tableView.delegate = self
         
 
         // Do any additional setup after loading the view.
@@ -28,4 +38,26 @@ class PickViewController: UIViewController {
     }
     */
 
+}
+
+extension PickViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CompanyTableViewCell", for: indexPath) as! CompanyTableViewCell
+        
+        cell.cLogoImg.image = UIImage(named: cLogos[indexPath.row])
+        cell.cName.text = cNames[indexPath.row]
+        cell.cKind.text = cKinds[indexPath.row]
+        
+        return cell
+     }
+    
+    
+}
+
+extension PickViewController : UITableViewDelegate {
+    
 }
