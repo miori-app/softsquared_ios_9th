244d2cd305b07251248f110e654cdc2a35cce225
iammiori <mioriii@naver.com>
2020-08-07 15:45:45 +0900

    [feat] add actionSheet destructive handler

--- SoftSquared_w03_wantedTable/SoftSquared_w03_wantedTable/FollowingCompanyVC.swift
+++ SoftSquared_w03_wantedTable/SoftSquared_w03_wantedTable/FollowingCompanyVC.swift
@@ -15,12 +15,26 @@ class FollowingCompanyVC: UIViewController {
     
     // 해당 cell의 indexPath.row에 맞는 action sheet 생성
     @objc func didTap(_ sender: UIButton){
-      // use the tag of button as index
-        let tapCName = cNames[sender.tag]
+        
+        // sender의 tag는 인덱스로 생각해도 될듯
+        let idx = sender.tag
+        
+        let tapCName = cNames[idx]
+        
+        
+        // ActionSheet 띄우기
         //첫번쨰 :  문구
         let optionMenu = UIAlertController(title: nil, message: "기업 '\(tapCName)' 팔로우 취소하시겠어요?", preferredStyle: .actionSheet)
         //두번째 : 액션 선택지
-        let deleteAction = UIAlertAction(title: "팔로우 취소", style: .default)
+        let deleteAction = UIAlertAction(title: "팔로우 취소", style: .destructive, handler: { (alert: UIAlertAction!) -> Void in
+            print("Yes")
+//            self.cNames.remove(at: idx)
+//            self.cLocs.remove(at: idx)
+//            self.cLogos.remove(at: idx)
+//            print(self.cNames)
+//            print("ok")
+        })
+        //let deleteAction = UIAlertAction(title: "팔로우 취소", style: .destructive, handler: )
         //세번째 : 밑에 취소 버튼
         let cancelAction = UIAlertAction(title: "취소", style: .cancel)
         
@@ -30,12 +44,23 @@ class FollowingCompanyVC: UIViewController {
         
         //마지막 : 보여줘!!
         self.present(optionMenu, animated: true, completion: nil)
-
+        
+        
+        
+        
     }
+    //
+    //    @objc func dataDelete(_ sender: UIButton){
+    //        let idx = sender.tag
+    //        cNames.remove(at: idx)
+    //        cLocs.remove(at: idx)
+    //        cLogos.remove(at: idx)
+    //        print(cNames)
+    //    }
     
-    let cNames = ["화해(버드뷰)","원티드랩","왓챠"]
-    let cLogos = ["birdview","wanted","watcha"]
-    let cLocs = ["서울특별시 마포구 와우산로 121","서울 송파구","서울 강남구"]
+    var cNames = ["화해(버드뷰)","원티드랩","왓챠"]
+    var cLogos = ["birdview","wanted","watcha"]
+    var cLocs = ["서울특별시 마포구 와우산로 121","서울 송파구","서울 강남구"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -86,7 +111,7 @@ extension FollowingCompanyVC : UITableViewDataSource {
         
         //didTap 함수 호출
         cell.followingBtn.addTarget(self, action: #selector(didTap(_:)), for: .touchUpInside)
-
+        
         
         return cell
     }
