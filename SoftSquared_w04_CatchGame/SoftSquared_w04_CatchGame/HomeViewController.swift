92d19412df774b4644641b34ace00e843e566ecd
iammiori <mioriii@naver.com>
2020-08-14 15:53:39 +0900

    [feat] Make MainHomeView and GameView (storyboard)

---
+++ SoftSquared_w04_CatchGame/SoftSquared_w04_CatchGame/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainGameViewController.swift
+//  SoftSquared_w04_CatchGame
+//
+//  Created by miori Lee on 2020/08/14.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
