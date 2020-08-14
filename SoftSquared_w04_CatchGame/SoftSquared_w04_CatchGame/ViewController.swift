67bf98c7168325b4a3cd1d8bcd35120d7e8eba1c
iammiori <mioriii@naver.com>
2020-08-14 15:10:32 +0900

    [init] week04 game project using thread

---
+++ SoftSquared_w04_CatchGame/SoftSquared_w04_CatchGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoftSquared_w04_CatchGame
+//
+//  Created by miori Lee on 2020/08/14.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
