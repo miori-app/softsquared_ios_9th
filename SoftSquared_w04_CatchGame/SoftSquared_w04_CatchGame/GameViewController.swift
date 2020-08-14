4e5e300290e5eb3c3a7741b1983c666891744b4d
iammiori <mioriii@naver.com>
2020-08-14 16:35:59 +0900

    [feat] Add chicken fall

--- SoftSquared_w04_CatchGame/SoftSquared_w04_CatchGame/GameViewController.swift
+++ SoftSquared_w04_CatchGame/SoftSquared_w04_CatchGame/GameViewController.swift
@@ -25,14 +25,31 @@ class GameViewController: UIViewController {
                 let randomNum = Int(arc4random_uniform(UInt32((Int)(UIScreen.main.bounds.size.width-100)))+1)
                 
                 var chickenbreast = UIImageView()
-                chickenbreast.image = UIImage(named: "Grilled-Chicken-Breast")
-                chickenbreast.frame = CGRect(x: randomNum + 30, y: 30, width: 30, height: 30)
-                
+                //chickenbreast.image = UIImage(named: "Grilled-Chicken-Breast")
+                //chickenbreast.frame = CGRect(x: randomNum + 30, y: 30, width: 40, height: 40)
+                self.ChickenMoveX(chickenimage: chickenbreast, randomNum: randomNum)
                 self.view.addSubview(chickenbreast)
+                
+                //chicken fall을 하고 싶어
+                Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { (t2) in
+                    //chickenbreast.center.y += 1
+                    self.ChickenFall(chickenimage: chickenbreast)
+                    
+                }
                 
             }
        
         
+    }
+    
+    func ChickenMoveX(chickenimage : UIImageView, randomNum : Int) {
+  
+        chickenimage.image = UIImage(named: "Grilled-Chicken-Breast")
+        chickenimage.frame = CGRect(x: randomNum + 30, y: 30, width: 40, height: 40)
+    }
+    
+    func ChickenFall(chickenimage : UIImageView) {
+        chickenimage.center.y += 1
     }
     
     
