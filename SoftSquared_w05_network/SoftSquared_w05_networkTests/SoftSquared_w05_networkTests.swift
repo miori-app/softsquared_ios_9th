5f5784b1604ad59061bebdb3b1d10b0d929da823
iammiori <mioriii@naver.com>
2020-08-20 05:06:08 +0900

    [init] network practice

---
+++ SoftSquared_w05_network/SoftSquared_w05_networkTests/SoftSquared_w05_networkTests.swift
@@ -0,0 +1,34 @@
+//
+//  SoftSquared_w05_networkTests.swift
+//  SoftSquared_w05_networkTests
+//
+//  Created by miori Lee on 2020/08/20.
+//  Copyright © 2020 miori. All rights reserved.
+//
+
+import XCTest
+@testable import SoftSquared_w05_network
+
+class SoftSquared_w05_networkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
