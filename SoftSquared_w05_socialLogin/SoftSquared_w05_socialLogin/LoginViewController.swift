2e6fed5c1e8493992ffc54461030685fe5ff94e9
iammiori <mioriii@naver.com>
2020-08-25 12:27:46 +0900

    [feat] add  naver button

--- SoftSquared_w05_socialLogin/SoftSquared_w05_socialLogin/LoginViewController.swift
+++ SoftSquared_w05_socialLogin/SoftSquared_w05_socialLogin/LoginViewController.swift
@@ -16,17 +16,26 @@ class LoginViewController: UIViewController {
     
     let loginInstance = NaverThirdPartyLoginConnection.getSharedInstance()
     
+    
+    //화면 구성
     private let loginButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("로그인", for: .normal)
+        let button = UIButton()
+        let image = UIImage(named: "naver_login") as UIImage?
+        button.setBackgroundImage(image, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        //button.setTitle("로그인", for: .normal)
         button.addTarget(self, action: #selector(touchUpLoginButton(_:)), for: .touchUpInside)
+        
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     private let logoutButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("로그아웃", for: .normal)
+        let image = UIImage(named: "logout") as UIImage?
+        button.setBackgroundImage(image, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        //button.setTitle("로그아웃", for: .normal)
         button.addTarget(self, action: #selector(touchUpLogoutButton(_:)), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -34,7 +43,7 @@ class LoginViewController: UIViewController {
     
     private let nameLabel: UILabel = {
         let label = UILabel()
-        label.text = "회원이름"
+        label.text = "name : "
         label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -42,7 +51,7 @@ class LoginViewController: UIViewController {
     
     private let emailLabel: UILabel = {
         let label = UILabel()
-        label.text = "이메일"
+        label.text = "email : "
         label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -50,11 +59,12 @@ class LoginViewController: UIViewController {
     
     private let nicknameLabel: UILabel = {
         let label = UILabel()
-        label.text = "별명"
+        label.text = "nickname : "
         label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
     
     
     
@@ -66,13 +76,23 @@ class LoginViewController: UIViewController {
         
     }
     
+    //로그인
     @objc private func touchUpLoginButton(_ sender: UIButton) {
         loginInstance?.delegate = self
         loginInstance?.requestThirdPartyLogin()
     }
     
+    //로그아웃
+    
     @objc private func touchUpLogoutButton(_ sender: UIButton) {
         loginInstance?.requestDeleteToken()
+        let alert = UIAlertController(title: "LOGOUT", message: "로그아웃!!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        self.nameLabel.text = "네이버"
+        self.emailLabel.text = "로그인을"
+        self.nicknameLabel.text = "해주세요"
     }
     
     private func getNaverInfo() {
@@ -93,6 +113,7 @@ class LoginViewController: UIViewController {
         
         //        let req = Alamofire.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: ["Authorization": authorization])
         
+        //alamofire 5 version으로 수정
         let req = AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: ["Authorization": authorization])
         req.responseJSON { response in
             guard let result = response.value as? [String: Any] else { return }
@@ -101,9 +122,9 @@ class LoginViewController: UIViewController {
             guard let email = object["email"] as? String else { return }
             guard let nickname = object["nickname"] as? String else { return }
             
-            self.nameLabel.text = "\(name)"
-            self.emailLabel.text = "\(email)"
-            self.nicknameLabel.text = "\(nickname)"
+            self.nameLabel.text = "name : \(name)"
+            self.emailLabel.text = "email : \(email)"
+            self.nicknameLabel.text = "nickname : \(nickname)"
         }
     }
     
@@ -116,13 +137,20 @@ class LoginViewController: UIViewController {
         view.addSubview(emailLabel)
         view.addSubview(nicknameLabel)
         
-        loginButton.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
-        loginButton.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -30).isActive = true
+        loginButton.leadingAnchor.constraint(equalTo: guide.leadingAnchor,constant: +70).isActive = true
+        loginButton.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -100).isActive = true
         
-        logoutButton.leadingAnchor.constraint(equalTo: loginButton.trailingAnchor).isActive = true
-        logoutButton.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
-        logoutButton.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -30).isActive = true
-        logoutButton.widthAnchor.constraint(equalTo: loginButton.widthAnchor).isActive = true
+        logoutButton.leadingAnchor.constraint(equalTo: guide.leadingAnchor,constant: +70).isActive = true
+        logoutButton.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -20).isActive = true
+        
+        //        logoutButton.leadingAnchor.constraint(equalTo: loginButton.trailingAnchor).isActive = true
+        //        logoutButton.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
+        //        logoutButton.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -30).isActive = true
+        //        logoutButton.widthAnchor.constraint(equalTo: loginButton.widthAnchor).isActive = true
+        
+        //loginButton.frame = CGRect(x: 89, y: 800, width: 236, height: 52)
+        //loginButton.layer.borderWidth = 1
+        //loginButton.layer.borderColor = UIColor.red.cgColor
         
         nameLabel.topAnchor.constraint(equalTo: guide.topAnchor, constant: 30).isActive = true
         nameLabel.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
@@ -137,40 +165,43 @@ class LoginViewController: UIViewController {
         nicknameLabel.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
         nicknameLabel.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
     }
+    
+    
 }
 
 extension LoginViewController: NaverThirdPartyLoginConnectionDelegate {
-  // 로그인 버튼을 눌렀을 경우 열게 될 브라우저
-  func oauth20ConnectionDidOpenInAppBrowser(forOAuth request: URLRequest!) {
-//     let naverSignInVC = NLoginThirdPartyOAuth20InAppBrowserViewController(request: request)!
-//     naverSignInVC.parentOrientation = UIInterfaceOrientation(rawValue: UIDevice.current.orientation.rawValue)!
-//     present(naverSignInVC, animated: false, completion: nil)
+    // 로그인 버튼을 눌렀을 경우 열게 될 브라우저
+    func oauth20ConnectionDidOpenInAppBrowser(forOAuth request: URLRequest!) {
+        //     let naverSignInVC = NLoginThirdPartyOAuth20InAppBrowserViewController(request: request)!
+        //     naverSignInVC.parentOrientation = UIInterfaceOrientation(rawValue: UIDevice.current.orientation.rawValue)!
+        //     present(naverSignInVC, animated: false, completion: nil)
+        
+        // UPDATE: 2019. 10. 11 (금)
+        // UIWebView가 제거되면서 NLoginThirdPartyOAuth20InAppBrowserViewController가 있는 헤더가 삭제되었습니다.
+        // 해당 코드 없이도 로그인 화면이 잘 열리는 것을 확인했습니다.
+    }
     
-    // UPDATE: 2019. 10. 11 (금)
-    // UIWebView가 제거되면서 NLoginThirdPartyOAuth20InAppBrowserViewController가 있는 헤더가 삭제되었습니다.
-    // 해당 코드 없이도 로그인 화면이 잘 열리는 것을 확인했습니다.
-  }
-  
-  // 로그인에 성공했을 경우 호출
-  func oauth20ConnectionDidFinishRequestACTokenWithAuthCode() {
-    print("[Success] : Success Naver Login")
-    getNaverInfo()
-  }
-  
-  // 접근 토큰 갱신
-  func oauth20ConnectionDidFinishRequestACTokenWithRefreshToken() {
+    // 로그인에 성공했을 경우 호출
+    func oauth20ConnectionDidFinishRequestACTokenWithAuthCode() {
+        print("[Success] : Success Naver Login")
+        getNaverInfo()
+        //loginButton.isHidden = true
+    }
     
-  }
-  
-  // 로그아웃 할 경우 호출(토큰 삭제)
-  func oauth20ConnectionDidFinishDeleteToken() {
-    loginInstance?.requestDeleteToken()
-  }
-  
-  // 모든 Error
-  func oauth20Connection(_ oauthConnection: NaverThirdPartyLoginConnection!, didFailWithError error: Error!) {
-    print("[Error] :", error.localizedDescription)
-  }
+    // 접근 토큰 갱신
+    func oauth20ConnectionDidFinishRequestACTokenWithRefreshToken() {
+        
+    }
+    
+    // 로그아웃 할 경우 호출(토큰 삭제)
+    func oauth20ConnectionDidFinishDeleteToken() {
+        loginInstance?.requestDeleteToken()
+    }
+    
+    // 모든 Error
+    func oauth20Connection(_ oauthConnection: NaverThirdPartyLoginConnection!, didFailWithError error: Error!) {
+        print("[Error] :", error.localizedDescription)
+    }
 }
 
 
